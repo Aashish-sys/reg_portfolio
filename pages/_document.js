6e4769b155f92bbc309e881affccab0c5d477bfc
Regulus <64154898+chethazz@users.noreply.github.com>
2023-04-01 22:25:06 +0530

    main: certain cleanups

--- pages/_document.js
+++ pages/_document.js
@@ -5,8 +5,8 @@ export default function Document() {
     <Html>
       <Head>
       <link rel="preconnect" href="https://fonts.googleapis.com" />
-<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
-<link href="https://fonts.googleapis.com/css2?family=Dancing+Script&family=Poppins&family=Rubik+Spray+Paint&family=Ubuntu&display=swap" rel="stylesheet" />
+      <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+      <link href="https://fonts.googleapis.com/css2?family=Dancing+Script&family=Poppins&family=Rubik+Spray+Paint&family=Ubuntu&display=swap" rel="stylesheet" />
       </Head>
       <body>
         <Main />
